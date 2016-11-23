e05b1996a547ea88e69120d6351e6785189cfcde
Manu <montes131@gmail.com>
2016-11-23 11:14:34 +0100

    close #9 Bug réglé

--- src/main/java/geco/LoginGenerator.java
+++ src/main/java/geco/LoginGenerator.java
@@ -36,7 +36,11 @@ public class LoginGenerator {
      */
     public String generateLoginForNomAndPrenom(String nom, String prenom) {
         String p = deAccent(prenom.substring(0,1).toUpperCase());
-        String n = deAccent(nom.substring(0,3).toUpperCase());
+        String n;
+        if(nom.length() > 2)
+            n = deAccent(nom.substring(0,3).toUpperCase());
+        else
+            n = deAccent(nom.substring(0,2).toUpperCase());
         String login = p+n ;
 
         if (loginService.loginExists(login)) {
